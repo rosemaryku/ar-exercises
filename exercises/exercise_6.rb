5f3a555aab366d043104f45ef1dd0a40f904b7a7
Rosemary Ku <rosemary.ku@hotmail.com>
2022-03-07 15:12:43 -0500

    ex 9 - one-to-many assoication

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Rosemary", last_name: "Ku", hourly_rate: 80)
+@store1.employees.create(first_name: "Chris", last_name: "Lam", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Jasmin", last_name: "Adams", hourly_rate: 20)
+@store2.employees.create(first_name: "James", last_name: "Smith", hourly_rate: 30)
+@store2.employees.create(first_name: "Louis", last_name: "Chan", hourly_rate: 40)
+
